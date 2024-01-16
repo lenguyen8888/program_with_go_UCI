119dff6058bd5ec3e668a3d72b666cb4b7e92da6
Nguyen Le <lenguyen_d@hotmail.com>
2024-01-16 00:31:44 -0800

    Update to remove go.mod

---
+++ golang-getting-started/module2/findian/findian.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Write a program which prompts the user to enter a string. The program searches
+// through the entered string for the characters ‘i’, ‘a’, and ‘n’.
+// The program should print “Found!” if the entered string starts with the character
+// ‘i’, ends with the character ‘n’, and contains the character ‘a’. The program should
+//  print “Not Found!” otherwise. The program should not be case-sensitive,
+// so it does not matter if the characters are upper-case or lower-case.
+
+func main() {
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a string:")
+	// Read all the input until a new line and store it in input
+	input, _ = reader.ReadString('\n')
+	// Remove all the spaces from the input
+	input = cleanUpInput(input)
+
+	found := true
+	// Check if the input starts with i
+	found = found && input[0] == 'i'
+	// Check if the input contains a
+	found = found && strings.Contains(input, "a")
+	// Check if the input ends with n
+	found = found && input[len(input)-1] == 'n'
+
+	if found {
+		fmt.Println("Found!")
+	} else {
+		fmt.Println("Not Found!")
+	}
+}
+
+func cleanUpInput(input string) string {
+	input = strings.TrimRight(input, "\n")
+	input = strings.Replace(input, " ", "", -1)
+	input = strings.TrimSpace(input)
+	input = strings.ToLower(input)
+	return input
+}
+
+// Examples: The program should print “Found!”
+// for the following example entered strings, “ian”, “Ian”, “iuiygaygn”, “I d skd a efju N”.
+// The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”.
